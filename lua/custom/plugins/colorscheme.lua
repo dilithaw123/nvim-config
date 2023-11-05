df8103b30113c731cc5ea16e6b0a9d63c2278c44
Dili <diliw101@gmail.com>
2023-11-06 12:51:09 +1300

    Light mode

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -13,7 +13,7 @@
 return {
   'nyoom-engineering/oxocarbon.nvim',
   config = function()
-    vim.cmd('set background=dark')
+    vim.cmd('set background=light')
     vim.cmd([[ colorscheme oxocarbon ]])
   end,
 }
