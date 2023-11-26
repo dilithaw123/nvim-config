a6e2ce876837696cca519d96bb233e61a32702bf
Dili <diliw101@gmail.com>
2023-11-27 00:13:37 +1300

    Update ui, add todo

--- init.lua
+++ init.lua
@@ -244,12 +244,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
+local actions = require('telescope.actions')
 require('telescope').setup {
   defaults = {
     mappings = {
       i = {
         ['<C-u>'] = false,
         ['<C-d>'] = false,
+        ['<S-BS>'] = actions.close,
       },
     },
   },
--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -11,5 +11,42 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-  }
+  },
+  config = function()
+    require("noice").setup({
+      presets = {
+        lsp_doc_border = true,
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "40%",
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "2%",
+          },
+        },
+        popupmenu = {
+          relative = "editor",
+          position = {
+            row = "48%",
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+          border = {
+            style = "rounded",
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+          },
+        },
+      },
+    })
+  end,
 }
