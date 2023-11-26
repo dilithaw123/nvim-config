d9b559fada9fcc042aa621bdf691a8a82bfb1a30
Dili <diliw101@gmail.com>
2023-11-27 09:35:05 +1300

    Disable copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,21 +1,22 @@
 return {
-  -- 'zbirenbaum/copilot.lua',
-  -- config = function()
-  --   require('copilot').setup({
-  --     panel = {
-  --       enabled = true,
-  --       auto_refresh = true,
-  --     },
-  --     suggestion = {
-  --       enabled = true,
-  --       auto_trigger = true,
-  --       keymap = {
-  --         accept_line = '<A-l>',
-  --         accept_word = '<A-h>',
-  --         next = '<A-k>',
-  --         prev = '<A-j>',
-  --       }
-  --     },
-  --   })
-  -- end,
+  'zbirenbaum/copilot.lua',
+  enabled = false,
+  config = function()
+    require('copilot').setup({
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept_line = '<A-l>',
+          accept_word = '<A-h>',
+          next = '<A-k>',
+          prev = '<A-j>',
+        }
+      },
+    })
+  end,
 }
