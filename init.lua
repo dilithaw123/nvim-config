c345664646070e8880ab7b603fa3754996bc9635
Dili <diliw101@gmail.com>
2024-05-19 19:39:55 +1200

    Fix gleam support

--- init.lua
+++ init.lua
@@ -403,7 +403,7 @@ vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope<CR>', { desc = '[S]earch [
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'zig', 'sql' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'zig', 'sql', 'gleam' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -532,20 +532,9 @@ require('which-key').register({
   ['<leader>h'] = { 'Git [H]unk' },
 }, { mode = 'v' })
 
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
 require('mason').setup()
 require('mason-lspconfig').setup()
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
-local servers = {
+local mason_servers = {
   gopls = {},
   rust_analyzer = {},
   tsserver = {},
@@ -557,10 +546,8 @@ local servers = {
     },
   },
 }
--- Setup neovim lua configuration
-require('neodev').setup()
 
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+require('neodev').setup()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
@@ -568,11 +555,12 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
+  ensure_installed = vim.tbl_keys(mason_servers),
 }
 
 mason_lspconfig.setup_handlers {
   function(server_name)
+    local servers = mason_servers
     local lspconfig = require 'lspconfig'
     lspconfig[server_name].setup {
       capabilities = capabilities,
@@ -582,6 +570,7 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+require('lspconfig').gleam.setup {}
 
 require('mason-tool-installer').setup {
   ensure_installed = {
