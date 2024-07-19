cd4293e1514fd9af46e5557fd5dbc66ab0a4b652
Dilitha <diliw101@gmail.com>
2024-07-19 15:14:50 +1200

    Fix lualine view

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -11,13 +11,16 @@ return {
       lualine_c = { { 'filename', file_status = true, path = 2 } },
     },
     tabline = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = {
+      lualine_a = {
         {
           'buffers',
+          symbols = {
+            alternate_file = '',
+          },
         },
       },
+      lualine_b = {},
+      lualine_c = {},
       lualine_x = {},
       lualine_y = {},
       lualine_z = {},
